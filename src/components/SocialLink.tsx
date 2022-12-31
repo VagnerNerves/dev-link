9d5f316cfd0ea89086545c57d3dce7519ad446ef
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-31 09:25:50 -0300

    feat: Create Component SocialLink.

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,24 @@
+import {
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoYoutube,
+  IoLogoLinkedin
+} from 'react-icons/io5'
+
+import styles from './SocialLink.module.css'
+
+interface SocialLinkProps {
+  url: string
+  typeLinks: 'github' | 'instragram' | 'youtube' | 'linkedin'
+}
+
+export function SocialLink({ url, typeLinks }: SocialLinkProps) {
+  return (
+    <a className={styles.socialLinks} href="">
+      {typeLinks == 'github' ? <IoLogoGithub size={24} /> : ''}
+      {typeLinks == 'instragram' ? <IoLogoInstagram size={24} /> : ''}
+      {typeLinks == 'youtube' ? <IoLogoYoutube size={24} /> : ''}
+      {typeLinks == 'linkedin' ? <IoLogoLinkedin size={24} /> : ''}
+    </a>
+  )
+}
