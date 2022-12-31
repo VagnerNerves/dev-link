db04b2693b736288cb3205a1f6332ef1095cdde7
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-31 11:35:32 -0300

    feat: Ajusted informations in App.

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ import { ToglleModeButtom } from './components/ToglleModeButtom'
 const links = [
   {
     id: 1,
-    url: '#',
-    name: 'Inscrever-se no NLW'
+    url: 'https://github.com/VagnerNerves',
+    name: 'Veja meu Portfólio'
   },
   {
     id: 2,
-    url: '#',
-    name: 'Baixar meu e-book'
+    url: 'https://vagnernerves.notion.site/Estudando-a7b8dcae8f044dbf88d523878785d5a8',
+    name: 'Minhas Anotações em Programação'
   },
   {
     id: 3,
-    url: '#',
-    name: 'Veja meu portfólio'
+    url: 'https://discord.gg/fbK5vb6PjB',
+    name: 'Meu Servidor Discord'
   },
   {
     id: 4,
-    url: '#',
-    name: 'Conheça o Explorer'
+    url: 'https://www.rocketseat.com.br/',
+    name: 'Conheça a Rocketseat'
   }
 ]
 
@@ -54,10 +54,27 @@ export function App() {
       </div>
 
       <div className={styles.socialLinks}>
-        <SocialLink url="#" typeLinks="Github" />
-        <SocialLink url="#" typeLinks="Instragram" />
-        <SocialLink url="#" typeLinks="Youtube" />
-        <SocialLink url="#" typeLinks="Linkedin" />
+        <SocialLink url="https://github.com/VagnerNerves" typeLinks="Github" />
+        <SocialLink
+          url="https://www.linkedin.com/in/vagnernervessantos/"
+          typeLinks="Linkedin"
+        />
+        <SocialLink
+          url="https://www.instagram.com/vagnernerves/"
+          typeLinks="Instragram"
+        />
+        <SocialLink
+          url="https://www.youtube.com/@VagnerNerves"
+          typeLinks="Youtube"
+        />
+        <SocialLink
+          url="https://www.twitch.tv/vagnernerves"
+          typeLinks="Twitch"
+        />
+        <SocialLink
+          url="https://twitter.com/VagnerNerves"
+          typeLinks="Twitter"
+        />
       </div>
 
       <footer>
