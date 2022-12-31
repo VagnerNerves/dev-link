80d29042cf8e514cd8935d62be8015aa60bb3a13
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-30 22:17:24 -0300

    feat: created Profile in top.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
-    <div className="App">
-      <p>Iniclized Project</p>
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        <img
+          src="https://github.com/VagnerNerves.png"
+          alt="Foto de Vagner Nerves"
+        />
+        <p>@VagnerNerves</p>
+      </div>
     </div>
   )
 }
