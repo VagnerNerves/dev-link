143ae3c4d65420d437f6d18af915c551aedb263f
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-31 11:12:02 -0300

    feat: Add new Social Links.

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,24 +1,48 @@
 import {
+  IoLogoTwitter,
+  IoLogoTwitch,
   IoLogoGithub,
+  IoLogoFacebook,
   IoLogoInstagram,
   IoLogoYoutube,
-  IoLogoLinkedin
+  IoLogoLinkedin,
+  IoLogoWhatsapp,
+  IoLogoTiktok
 } from 'react-icons/io5'
 
 import styles from './SocialLink.module.css'
 
 interface SocialLinkProps {
   url: string
-  typeLinks: 'github' | 'instragram' | 'youtube' | 'linkedin'
+  typeLinks:
+    | 'Twitter'
+    | 'Twitch'
+    | 'Github'
+    | 'Facebook'
+    | 'Instragram'
+    | 'Youtube'
+    | 'Linkedin'
+    | 'Whatsapp'
+    | 'Tiktok'
 }
 
 export function SocialLink({ url, typeLinks }: SocialLinkProps) {
   return (
-    <a className={styles.socialLinks} href={url} target="_blank">
-      {typeLinks == 'github' ? <IoLogoGithub size={24} /> : ''}
-      {typeLinks == 'instragram' ? <IoLogoInstagram size={24} /> : ''}
-      {typeLinks == 'youtube' ? <IoLogoYoutube size={24} /> : ''}
-      {typeLinks == 'linkedin' ? <IoLogoLinkedin size={24} /> : ''}
+    <a
+      className={styles.socialLinks}
+      href={url}
+      target="_blank"
+      title={typeLinks}
+    >
+      {typeLinks == 'Twitter' ? <IoLogoTwitter size={24} /> : ''}
+      {typeLinks == 'Twitch' ? <IoLogoTwitch size={24} /> : ''}
+      {typeLinks == 'Github' ? <IoLogoGithub size={24} /> : ''}
+      {typeLinks == 'Facebook' ? <IoLogoFacebook size={24} /> : ''}
+      {typeLinks == 'Instragram' ? <IoLogoInstagram size={24} /> : ''}
+      {typeLinks == 'Youtube' ? <IoLogoYoutube size={24} /> : ''}
+      {typeLinks == 'Linkedin' ? <IoLogoLinkedin size={24} /> : ''}
+      {typeLinks == 'Whatsapp' ? <IoLogoWhatsapp size={24} /> : ''}
+      {typeLinks == 'Tiktok' ? <IoLogoTiktok size={24} /> : ''}
     </a>
   )
 }
